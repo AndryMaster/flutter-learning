b21819da50b2acf7ebaa1ed3f17e20f07bd7fded
AndryMaster <rusandrkozak@gmail.com>
2026-05-08 16:19:48 +0300

    up to 3.5

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TextGeometryExample(),
+      theme: ThemeData.light(),
+      home: const CurvesExampleWrapper(),
     );
   }
 }
 
-class TextGeometryExample extends StatefulWidget {
-  const TextGeometryExample({super.key});
+class CurvesExample extends StatelessWidget {
+  final bool selected;
+  final Color color;
+  final Curve curve;
 
-  @override
-  State<TextGeometryExample> createState() => _TextGeometryExampleState();
-}
-
-class _TextGeometryExampleState extends State<TextGeometryExample> {
-  final GlobalKey textKey = GlobalKey();
-  Size size = const Size(0, 0);
-
-  @override
-  void initState() {
-    super.initState();
-
-    /// Добавляем [postFrameCallback] в [SchedulerBinding]
-    /// Он вызовется после отрисовки текущего кадра, в частности после
-    /// отрисовки текста с [GlobalKey] = [textKey].
-    ///
-    /// Размеры виджета text будут известны
-    /// в момент исполнения [_changeAnimatedContainerDimensions],
-    /// тк он вызывается ПОСЛЕ отрисовки кадра, т.е. размер текста будет посчитан
-    SchedulerBinding.instance
-        .addPostFrameCallback(_changeAnimatedContainerDimensions);
-
-    /// Аналогичного результата можно достичь используя Future api
-    /// и геттер [endOfFrame] у [SchedulerBinding].
-    ///
-    /// Future будет завершен тогда, когда завершится отрисовка текущего кадра
-    ///
-    /// Пример:
-    /// SchedulerBinding.instance.endOfFrame
-    ///     .then((_) => _changeAnimatedContainerDimensions());
-  }
-
-  void _changeAnimatedContainerDimensions(
-      [Duration? postframeCallbackDuration]) {
-    /// Получаем [RenderObject] который относится к виджету Text.
-    /// После отрисовки в нем есть информация о геометрии виджета.
-    RenderBox logoBox = textKey.currentContext!.findRenderObject() as RenderBox;
-
-    /// Получаем размер
-    size = Size(
-      logoBox.size.width + 5,
-      logoBox.size.height + 5,
-    );
-
-    /// Обновляем стейт. Изменения размера [size] спровоцирует анимацию у [AnimatedContainer]
-    setState(() {});
-  }
+  const CurvesExample({
+    required this.selected,
+    required this.color,
+    required this.curve,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    /// [Stack] используется намеренно, чтобы продемонстрировать возможность
-    /// получить размер виджета, расположенного параллельно в дереве.
-    ///
-    /// Данного эффекта можно добиться и не используя [postFrameCallback]
-    return Scaffold(
-      body: Stack(
-        children: [
-          /// Контейнер с конкретным размером. Ожидается что в переменной size
-          /// будет размер текста, расположенного ниже по стеку.
-          ///
-          /// Текст в виджете ниже может быть произвольный и на момент верстки
-          /// мы не можем точно знать его размер.
-          Center(
-            child: AnimatedContainer(
-              duration: const Duration(seconds: 2),
-              curve: Curves.bounceInOut,
-              width: size.width,
-              height: size.height,
-              color: Colors.amber,
-            ),
+    return AnimatedAlign(
+      alignment: selected ? Alignment.bottomCenter : Alignment.topCenter,
+      duration: const Duration(seconds: 1),
+      curve: curve,
+      child: GestureDetector(
+        child: Card(
+          color: color,
+          child: const SizedBox(
+            width: 100.0,
+            height: 100.0,
           ),
-          Center(
-            child: Text(
-              key: textKey,
-              'Динамический текст',
-              style: const TextStyle(fontSize: 20),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
+
+class CurvesExampleWrapper extends StatefulWidget {
+  const CurvesExampleWrapper({super.key});
+
+  @override
+  State<CurvesExampleWrapper> createState() => _CurvesExampleWrapperState();
+}
+
+class _CurvesExampleWrapperState extends State<CurvesExampleWrapper> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 116.0),
+                    child: CurvesExample(
+                      selected: selected,
+                      curve: MyLinearCurve(),
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 116.0),
+                    child: CurvesExample(
+                      selected: selected,
+                      curve: Curves.easeIn,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  selected = !selected;
+                });
+              },
+              child: const Text('Анимировать'),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Созданная нами Curves.linear
+class MyLinearCurve extends Curve {
+  @override
+  double transformInternal(double t) => t;
+}
+
